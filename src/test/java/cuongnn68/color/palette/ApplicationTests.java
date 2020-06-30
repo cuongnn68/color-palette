aab1027fbdd886525afb85143fa5d864a8051c7a
cuongnn <nncuong.nnc@gmail.com>
2020-06-30 09:15:24 +0700

    first

---
+++ src/test/java/cuongnn68/color/palette/ApplicationTests.java
@@ -0,0 +1,13 @@
+package cuongnn68.color.palette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
